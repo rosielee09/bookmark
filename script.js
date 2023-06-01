0fe9850bd787e0f51b5d871b84640e3bd2cad5ff
rosie <rosie.bh.lee@gmail.com>
2023-05-31 17:06:14 -0700

    local storage

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const modalClose = document.getElementById('close-modal');
 const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
-const bookmarksContainer = document.getElementById('bookmarks-container');
+let bookmarks = [];
 
 // Show Modal, Focus on Input
 function showModal() {
@@ -16,3 +16,65 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', ()=> modal.classList.remove('show-modal'));
 window.addEventListener('click', (e)=>(e.target === modal? modal.classList.remove('show-modal') : false));
+
+// Validate Form
+function validate(nameValue, urlValue){
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if(!nameValue || !urlValue) {
+    alert('Please submit values sfor both fields.');
+    return false;
+  }
+  if(urlValue.match(regex)){
+    alert('Please provide a valid web address');
+    return false;
+  }
+  // Valid
+  return true;
+}
+
+// Fetch Bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from localStorage if available
+  if(localStorage.getItem('bookmarks')){
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  } else {
+    //Create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: 'Hero Patterns',
+        url: 'https://heropatterns.com/'
+      }
+    ];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+}
+
+// Handle Data from Form
+function storeBookmark(e){
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes('http://', 'https://')){
+    urlValue = `https://${urlValue}`;
+  }
+  console.log(nameValue, urlValue);
+  if(!validate(nameValue, urlValue)){
+    return false;
+  }
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
+
+}
+
+// Event Listner
+bookmarkForm.addEventListener('submit', storeBookmark);
+// On Load, feth bookamrks
+fetchBookmarks();
